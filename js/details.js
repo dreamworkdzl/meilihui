51d550197fd21ad442f0bf9c222dbabd9a3382d8
Your Name <you@example.com>
2017-11-22 17:47:01 +0800

    五个页面完成

--- js/details.js
+++ js/details.js
@@ -28,7 +28,71 @@ $(function(){
 })
 
 /*******		产品页面		*******/
+$(function(){
 
-
-
-
+	var str ='';
+	$.ajax("json/details.json")
+	
+	.then(function(res){
+		for(var i = 0;i < 36;i++){
+			str += `<div class="product">
+						<div class="product_img">
+							<a href="#" style="z-index:3"><img src=${res[i].imgBig1}/></a>
+							<a href="#" style="z-index:2"><img src=${res[i].imgBig2}/></a>
+							<a href="#" style="z-index:1"><img src=${res[i].imgBig3}/></a>
+						</div>
+						<div class="product_pop">
+							<div class="product_pop_con">
+								<div class="product_cen">
+									<span class="product_pop_span product_pop_span1" style="z-index:2"><img src=${res[i].imgSmall1}/></span>
+									<span class="product_pop_span"><img src=${res[i].imgSmall2}/></span>
+									<span class="product_pop_span"><img src=${res[i].imgSmall3}/></span>
+								</div>
+							</div>
+						</div>
+						<div class="product_con">
+							<p style="display: block; width: 225px;  overflow:  hidden; white-space: nowrap; -o-text-overflow: ellipsis; text-overflow:  ellipsis;">
+								<a href="#">MASON GARMENTS</a>
+							</p>
+							<pstyle="display: block; width: 225px;  overflow:  hidden; white-space: nowrap; -o-text-overflow: ellipsis; text-overflow:  ellipsis;">
+								<a href="#">${res[i].name}</a>
+							</p>
+							<p class="product_p">
+								<span class="product_span">${res[i].price}</span>
+								<span style="text-decoration:line-through;font-size:12px;">${res[i].prices}</span>
+							</p>
+						</div>
+					</div>`;
+		}
+		$(".the_list").html(str);
+		
+		
+		$(".product").mouseover(function(){
+			$(this).children(1).css({"display":"block"})
+		}).mouseout(function(){
+			$(".product_pop").css({"display":"none"})
+		})
+		
+		$(".product_cen span").mouseover(function(){
+			var index = $(this).index();
+			$(this).css({"border":"1px solid #000"})
+			//alert($(this).index())
+//			$(".product_img a")
+			$(this).parent().parent().parent().prev().children()					
+								.eq(index)
+								.show()
+								.siblings()
+								.hide()
+								
+		}).mouseout(function(){
+			$(this).css({"border":"0"})
+			$(this).parent().parent().parent().prev().children().index(0)
+		})
+	})
+		
+	
+	
+	
+	
+	
+})
